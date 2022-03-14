dee491704a34724802ec80fb245fd973e3e190ad
sergio-rins <sergiorins@gmail.com>
2022-03-14 16:47:33 +0000

    updates

--- ietimoodle/models.py
+++ ietimoodle/models.py
@@ -28,6 +28,7 @@ class NivelPrivacidad(models.Model):
     descripcion = models.CharField(max_length=255)
     def __str__(self):
         return self.nombre
+        
 class User(AbstractUser):
   nombre = models.CharField(max_length = 50, unique=True)
   correo = models.EmailField(max_length=254, unique = True)
