a778cd98d289ba06beb21f3cf8631acd7763fc9c
sergio-rins <sergiorins@gmail.com>
2022-03-16 17:56:53 +0000

    todo

--- ietimoodle/urls.py
+++ ietimoodle/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('<int:exerciseid>/<int:alumnid>', views.index, name='index'),
 ]
--- ietimoodle/views.py
+++ ietimoodle/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,  get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse
 
+from .models import *
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the moodle index.")
+def index(request, exerciseid, alumnid):
+    alumn= get_object_or_404(User, pk=alumnid)
+    exercise=get_object_or_404(Ejercicio, pk=exerciseid)
+    delivery=Entrega.objects.filter(ejercicio=exercise.pk, user=alumn)[0]
+    nextAlumn=alumnid+1
+    prevAlumn=alumnid-1
+    content = {
+        'alumn': alumn,
+        'exercise': exercise,
+        'delivery': delivery,
+        'nextAlumn': nextAlumn,
+        'prevAlumn': prevAlumn,
+    }
+    return render(request, 'index.html', content)
