50f894b729fd75033c6612179fb897a53b132e37
Guillem Albo Pintor <albopintor@gmail.com>
2022-03-16 18:54:08 +0100

    update inline

---
+++ ietimoodle/admin.py
@@ -0,0 +1,64 @@
+from django.contrib import admin
+from .models import *
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin 
+from django.conf import settings
+from django.contrib.auth.forms import UserChangeForm
+from django.utils.translation import ugettext_lazy as _
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    fieldsets = (
+      (None, {'fields': ('username','correo', 'password', )}),
+      (_('Información Personal'), {'fields': ('first_name', 'last_name')}),
+      (_('Permisos'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                     'groups', 'user_permissions')}),
+      (_('Datos Relevantes'), {'fields': ('last_login', 'date_joined')}),
+        (_('Información Usuario'), {'fields': ('centro', 'privacidad')}),
+    )
+    add_fieldsets = (
+      (None, {
+          'classes': ('wide', ),
+          'fields': ('username','correo', 'password1', 'password2'),
+      }),
+    )
+    list_display = ['username','correo', 'first_name', 'last_name', 'is_staff']
+    search_fields = ('username','correo', 'first_name', 'last_name')
+    ordering = ('correo', )
+
+class SuscripiconesInLine(admin.TabularInline):
+    model = Suscripcion
+    extra = 1
+    
+
+class EjercicioInLine(admin.TabularInline):
+    model = Ejercicio
+    extra = 1
+class EntrgaInLine(admin.TabularInline):
+    model = Entrega
+    extra = 1
+class RecursoInLine(admin.TabularInline):
+    model = Recurso
+    extra = 1
+
+
+
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('nombre','descripcion','centro')
+    inlines =[
+        SuscripiconesInLine,
+        EjercicioInLine,
+        EntrgaInLine,
+        RecursoInLine
+        
+    ]
+    
+
+
+admin.site.register(Centro) 
+admin.site.register(Curso,CursoAdmin)
+admin.site.register(Ejercicio)
+admin.site.register(NivelPrivacidad) 
+admin.site.register(User, UserAdmin)
+admin.site.register(Entrega) 
+admin.site.register(Suscripcion)
+admin.site.register(Recurso)
---
+++ ietimoodle/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+# Create your models here.
+class Centro(models.Model):
+    nombre = models.CharField(max_length=200)
+    localitat = models.CharField(max_length=200)
+    def __str__(self):
+        return self.nombre
+class Curso(models.Model):
+    nombre = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=255)
+    centro = models.ForeignKey(Centro, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.nombre
+class Ejercicio(models.Model):
+    nombre = models.CharField(max_length=200)
+    ponderacion = models.IntegerField(default=0)
+    visibilidad = models.BooleanField(default=False)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.nombre
+
+class NivelPrivacidad(models.Model):
+    nombre = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=255)
+    def __str__(self):
+        return self.nombre
+        
+        
+class User(AbstractUser):
+  correo = models.EmailField(max_length=254, unique = True)
+  centro = models.ForeignKey(Centro, on_delete=models.CASCADE, null=True, blank=True)
+  privacidad = models.ForeignKey(NivelPrivacidad, on_delete=models.CASCADE, null=True, blank=True)
+
+  REQUIRED_FIELDS = ['first_name', 'last_name']
+  def __str__(self):
+      return "{}".format(self.username)
+
+class Entrega(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+
+    ejercicio = models.ForeignKey(Ejercicio, on_delete=models.CASCADE)
+    estado = models.BooleanField()
+    cualificacion = models.IntegerField(default=0)
+    archivo = models.FileField(upload_to="./archivos/entregas/")
+    fecha_entrega = models.DateTimeField()
+    comentario_profesor = models.CharField(max_length=255)
+    comentario_alumno = models.CharField(max_length=255)
+    def __str__(self):
+        return self.ejercicio.nombre
+
+class Suscripcion(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    tipo = models.CharField(max_length=200)
+    def __str__(self):
+        return self.tipo
+
+class Recurso(models.Model):
+    titulo = models.CharField(max_length=200)
+    texto = models.CharField(max_length=255, blank=True)
+    archivo = models.FileField(upload_to="./archivos/recursos/", blank=True)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)                                            
+    def __str__(self):
+        return self.titulo
