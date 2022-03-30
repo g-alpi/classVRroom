7b7c19af0bf8a42789d44a17a3bfe150bbe4c55e
Guillem Albo Pintor <albopintor@gmail.com>
2022-03-31 01:13:06 +0200

    Update backends.py

--- ietimoodle/backends.py
+++ ietimoodle/backends.py
@@ -9,12 +9,12 @@ UserModel = get_user_model()
 class EmailBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         try:
-            user = UserModel.objects.get(Q(correo__iexact=username|Q(username__iexact=username))) #|Q(username__iexact=username)
+            user = UserModel.objects.get(Q(correo__iexact=username | Q(username__iexact=username))) #|Q(username__iexact=username)
         except UserModel.DoesNotExist:
             UserModel().set_password(password)
             return
         except UserModel.MultipleObjectsReturned:
-            user = UserModel.objects.filter(Q(correo__iexact=username|Q(username__iexact=username))).order_by('id').first() #|Q(username__iexact=username)
+            user = UserModel.objects.filter(Q(correo__iexact=username | Q(username__iexact=username))).order_by('id').first() #|Q(username__iexact=username)
 
         if user.check_password(password) and self.user_can_authenticate(user):
             return user
