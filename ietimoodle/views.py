b547d8a4358acc3aace4b50a5e6c418813c2171a
Guillem Albo Pintor <albopintor@gmail.com>
2022-03-31 02:28:22 +0200

    update

--- ietimoodle/views.py
+++ ietimoodle/views.py
@@ -74,7 +74,6 @@ def grade(request, cursoid):
 	recursos = Recurso.objects.filter(curso=cursoid)
 	ejercicios = Ejercicio.objects.filter(curso=cursoid)
 	role= Suscripcion.objects.filter(curso=cursoid, user=request.user.pk)[0]
-	suscripciones=Suscripcion.objects.filter(curso=cursoid)[0]
 	alumnos= Suscripcion.objects.filter(curso=cursoid,tipo='alumno')[0]
 	firstAlId=get_object_or_404(User, pk=alumnos.user.pk)
 	content= {
