a9b6c90ec8f2852f7a4cc3034d7c3d6a86b5c56e
sergio-rins <sergiorins@gmail.com>
2022-04-04 13:36:18 +0000

    the models

--- ietimoodle/models.py
+++ ietimoodle/models.py
@@ -18,16 +18,30 @@ class Curso(models.Model):
         return self.nombre
     def toJson(self):
         return json.dumps(self, default=lambda o: o.__dict__)
-
+    
 class Ejercicio(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=255)
+    def __str__(self):
+        return f'{self.pk} - {self.nombre}'
+
+class Tarea(models.Model):
     nombre = models.CharField(max_length=200)
     ponderacion = models.IntegerField(default=0)
     visibilidad = models.BooleanField(default=False)
     curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
     minVersion = models.CharField(max_length=200, null=True, blank=True)
+    ejercicio = models.ForeignKey(Ejercicio, on_delete=models.CASCADE, blank=True)
     def __str__(self):
         return self.nombre
 
+class VRTarea(models.Model):
+    ejercicio = models.ForeignKey(Ejercicio, on_delete=models.CASCADE)
+    minversion = models.CharField(max_length=255, null=True, blank=True)
+    autograde = models.CharField(max_length=255, null=True, blank=True)
+    version = models.CharField(max_length=255, null=True, blank=True)
+    performance_data = models.CharField(max_length=255, null=True, blank=True)
+ 
 class NivelPrivacidad(models.Model):
     nombre = models.CharField(max_length=200)
     descripcion = models.CharField(max_length=255)
@@ -46,16 +60,14 @@ class User(AbstractUser):
 class Entrega(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
-    ejercicio = models.ForeignKey(Ejercicio, on_delete=models.CASCADE)
-    cualificado = models.BooleanField()
-    cualificacion = models.IntegerField(default=0)
+    tarea = models.ForeignKey(Tarea, on_delete=models.CASCADE, null=True, blank=True)
+    vrtarea = models.ForeignKey(VRTarea, on_delete=models.CASCADE, nul=True, blank=True)
     archivo = models.FileField(upload_to="./archivos/entregas/",blank=True)
     fecha_entrega = models.DateTimeField()
-    comentario_profesor = models.CharField(max_length=255, null=True, blank=True)
-    comentario_alumno = models.CharField(max_length=255, null=True, blank=True)
-    pin = models.CharField(max_length=4, null=True, blank=True)
+    pin = models.IntegerField(null=True, blank=True)
+
     def __str__(self):
-        return self.ejercicio.nombre
+        return self.tarea.nombre
 
 class Suscripcion(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -71,3 +83,15 @@ class Recurso(models.Model):
     curso = models.ForeignKey(Curso, on_delete=models.CASCADE)                                            
     def __str__(self):
         return self.titulo
+
+class Calificacion(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    tarea = models.ForeignKey(Tarea, on_delete=models.CASCADE)
+    nota = models.IntegerField(null=True)
+    fecha_entrega = models.DateTimeField()
+    comentario_profesor = models.CharField(max_length=255, null=True, blank=True)
+    comentario_alumno = models.CharField(max_length=255, null=True, blank=True)
+    def str(self):
+        return '{}{}'.format(self.nota, self.user)
+
+
